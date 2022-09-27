835661f37d81b01a6cb3d76d5885155868afcd93
Pocevicius <lukas.pocevicius@gmail.com>
2022-09-27 21:16:23 +0300

    updated to dev branch

---
+++ src/getFullAvatars.js
@@ -0,0 +1,9 @@
+import { avatar } from "./avatars.js";
+import { sword } from "./weapons";
+
+const getFullAvatars = () => {
+  console.log(avatar);
+  console.log(sword);
+};
+
+export default getFullAvatars;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import getFullAvatars from "./getFullAvatars";
+
+console.log("viskas veikia");
+getFullAvatars();
+
+const first = { a: 1, b: 2 };
+const second = { ...first, c: 3 };
+
+console.log(second);
