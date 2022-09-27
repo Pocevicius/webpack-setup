55ec0801dd1a666b899f3a7816f6f28e7dde42cc
Lukas <104161208+Pocevicius@users.noreply.github.com>
2022-09-27 21:20:09 +0300

    updated to dev branch (#1)

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+const mode =
+  process.env.NODE_ENV === "production" ? "production" : "development";
+console.log(mode);
+
+module.exports = {
+  mode: mode,
+
+  entry: path.resolve(__dirname, "./src/index.js"),
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          //without additional babel settings,this will reference.
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  devtool: "source-map",
+
+  devServer: {
+    static: "./public",
+  },
+};
